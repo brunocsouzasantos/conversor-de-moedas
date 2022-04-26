d3c30c2a054ac8431e3fb60d343748b32e532bd5
Bruno Santos <brunocsouzasantos@gmail.com>
2022-04-26 12:23:10 -0300

    primeiro commit

--- app.js
+++ app.js
@@ -32,6 +32,9 @@
 const  currencyOneEl = document.querySelector('[data-js="currency-one"]')
 const  currencyTwoEl = document.querySelector('[data-js="currency-two"]')
 
-currencyOneEl.innerHTML = `<option> oi </option>`
+const option = `<option> oi </option>`
+
+currencyOneEl.innerHTML = option
+currencyTwoEl.innerHTML = option
 
 console.log(currencyOneEl, currencyTwoEl)
